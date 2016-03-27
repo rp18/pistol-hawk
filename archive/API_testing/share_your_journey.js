deec69e4973cfc1b1844231931eb081db9263f15
Michael Daof <michael.ivanhoe@gmail.com>
2016-03-27 00:47:25 -0700

    Cleaned up files for push; archived non-use files

--- archive/API_testing/share_your_journey.js
+++ archive/API_testing/share_your_journey.js
@@ -1,29 +1,5 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
-// creates user list in HTML
-createList(userIndex);
-
-// onClick to draw all users
-$('#all-users').on("click", function(){
-  drawAllUsers(userIndex);
-  $("#story").empty();
-});
-
-// onClick for user actions; including map draw and story/metadata
-$('#user_list > ul > li').on("click", function(){
-  var id = $(this).attr("id")
-  userIndex.forEach(function(user, index){
-    if (user.id == id){
-      drawOneUser(user);
-      $("#story").html("<div class='story-style col-sm-3'><blockquote><img class='thumbnail' src='" + user.image + "' alt=''><p>" + user.storyText + "</p></blockquote></div>");
-    }; // if
-  }); // .forEach
-}); // onClick for user actions
-
-
-}); // .ready()
-=======
   var map = new GMaps({
     div: '#map',
     zoom: 8,
@@ -43,7 +19,17 @@ $('#user_list > ul > li').on("click", function(){
       travelMode: 'driving',
       strokeColor: 'green'
     },
-    storyText: "Well, we anchored ship in Miami, but all the party people got on my nerves. So I decided to take my crew and trek them inland. We passed the Everglades and found a remote clearing. A perfect spot to temporarily hide my treasure. I mean, to not hide my treasure. Cuz I didn't hide any treasure there. But even if I did; I'd slit your throat if you touched it."
+    storyText: "Well, we anchored ship in Miami, but all the party people got on my nerves. So I decided to take my crew and trek them inland. We passed the Everglades and found a remote clearing. A perfect spot to temporarily hide my treasure. I mean, to not hide my treasure. Cuz I didn't hide any treasure there. But even if I did; I'd slit your throat if you touched it.",
+    overlayText: [{
+      lat: 26.239111, 
+      lng: -80.481194,
+      content: '<div class="overlay">Stopped to pee.</div>'
+    },
+    {
+      lat: 27.059008, 
+      lng: -81.437004,
+      content: '<div class="overlay">Miami, baby!!!.</div>'
+    }] // overlay array
   };
 
   var user101 = {
@@ -56,7 +42,8 @@ $('#user_list > ul > li').on("click", function(){
       travelMode: 'driving',
       strokeColor: 'red'
     },
-    storyText: "After three straight weeks of drinking rum and partying at West Palm Beach, we decided to trek back to our ship on the other side of the peninsula, but we got stuck in the swamps. We now need help. Really, this isn't a journal entry. Somebody please send help."
+    storyText: "After three straight weeks of drinking rum and partying at West Palm Beach, we decided to trek back to our ship on the other side of the peninsula, but we got stuck in the swamps. We now need help. Really, this isn't a journal entry. Somebody please send help.",
+    overlayText: []
   };
 
   userIndex.push(user100);
@@ -73,12 +60,25 @@ $('#user_list > ul > li').on("click", function(){
     }; // drawAllUsers()
 
     var drawOneUser = function drawOneUser(user){
+      // tofix: this map assignment method refreshes entire page. makes entire drawOneUser function annoying.
+      // this map assignment is here to erase previous renderings. a bit crude.
+      // map = GMaps({
+      //   div: '#map',
+      //   zoom: 8,
+      //   lat: 26.280131, 
+      //   lng: -81.033708
+      // });
+      // cerntainly simpler, but page-refresh problem remains.
       map.removePolylines();
+      map.removeOverlays();
       map.drawRoute({
         origin: user.drawProps.origin,
         destination: user.drawProps.destination,
         strokeColor: user.drawProps.strokeColor
         }); // map.drawRoute()
+      user.overlayText.forEach(function(key){
+        map.drawOverlay(key);
+      }); //
     }; // drawOneUser()
 
 
@@ -90,32 +90,35 @@ $('#user_list > ul > li').on("click", function(){
         )
     };
 
+  // list id functions 
+
+    // $('#100').click(function() {
+    //   drawOneUser(user100);
+    //   $("#story").html("<div class='story-style'><p>" + user100.storyText + "</p></div>"); 
+    // });
+
+    // $('#101').click(function() {
+    //   drawOneUser(user101);
+    //   $("#story").html("<div class='story-style'><p>" + user101.storyText + "</p></div>"); 
+    // });
+
     $('#all-users').on("click", function(){
       drawAllUsers(userIndex);
       $("#story").empty();
     });
-
-    // $('#user_list > ul > li').on("click", function(){
-    //   var id = $(this).attr("id")
-    //   userIndex.forEach(function(user, index){
-    //     if (user.id == id){
-    //       drawOneUser(user);
-    //       $("#story").html("<div class='story-style col-sm-3'><blockquote><p>" + user.storyText + "</p></blockquote></div>");
-    //       }; // if
-    //     }); // .forEach
-    // });
 
     $('#user_list > ul > li').on("click", function(){
       var id = $(this).attr("id")
       userIndex.forEach(function(user, index){
         if (user.id == id){
           drawOneUser(user);
-          $("#story").html("<div class='story-style col-sm-3'><blockquote><img class='thumbnail' src='" + user.image + "' alt=''><p>" + user.storyText + "</p></blockquote></div>");
+          $("#story").html("<div class='story-style col-sm-3'><blockquote><p>" + user.storyText + "</p></blockquote></div>");
           }; // if
         }); // .forEach
     });
 
 
+
+
 }); // .ready()
 
->>>>>>> 8819185e74ac2585b5486ca74950f92af9365427
--- archive/journey_share.js
+++ archive/journey_share.js
@@ -1,34 +1,16 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
-// creates user list in HTML
-createList(userIndex);
-
-// onClick to draw all users
-$('#all-users').on("click", function(){
-  drawAllUsers(userIndex);
-  $("#story").empty();
-});
-
-// onClick for user actions; including map draw and story/metadata
-$('#user_list > ul > li').on("click", function(){
-  var id = $(this).attr("id")
-  userIndex.forEach(function(user, index){
-    if (user.id == id){
-      drawOneUser(user);
-      $("#story").html("<div class='story-style col-sm-3'><blockquote><img class='thumbnail' src='" + user.image + "' alt=''><p>" + user.storyText + "</p></blockquote></div>");
-    }; // if
-  }); // .forEach
-}); // onClick for user actions
-
-
-}); // .ready()
-=======
   var map = new GMaps({
     div: '#map',
     zoom: 8,
     lat: 26.280131, 
-    lng: -81.033708
+    lng: -81.033708,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_RIGHT
+    },
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_BOTTOM
+    },
   });
 
   var userIndex = [];
@@ -59,8 +41,36 @@ $('#user_list > ul > li').on("click", function(){
     storyText: "After three straight weeks of drinking rum and partying at West Palm Beach, we decided to trek back to our ship on the other side of the peninsula, but we got stuck in the swamps. We now need help. Really, this isn't a journal entry. Somebody please send help."
   };
 
+  var user102 = {
+    name: 'Jack Kirby',
+    id: '102',
+    image: "http://40.media.tumblr.com/tumblr_md0rkp1n9k1qhpx4lo1_1280.jpg",
+    drawProps: {
+      origin: [26.696427, -80.146111],
+      destination: [26.455702, -81.887443],
+      travelMode: 'driving',
+      strokeColor: 'purple'
+    },
+    storyText: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source"
+  };
+
+  var user103 = {
+    name: 'Iron Man',
+    id: '103',
+    image: "http://media.moddb.com/images/downloads/1/60/59123/NEFDyIy3fCgkIM_1_1.jpg",
+    drawProps: {
+      origin: [24.626925, -81.579826],
+      destination: [25.829443, -81.316154],
+      travelMode: 'driving',
+      strokeColor: 'black'
+    },
+    storyText: "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.",
+  };
+
   userIndex.push(user100);
   userIndex.push(user101);
+  userIndex.push(user102);
+  userIndex.push(user103);
 
   var drawAllUsers = function drawAllUsers(index){
     index.forEach(function(user){
@@ -118,4 +128,3 @@ $('#user_list > ul > li').on("click", function(){
 
 }); // .ready()
 
->>>>>>> 8819185e74ac2585b5486ca74950f92af9365427
